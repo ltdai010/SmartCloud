16e5a3e55510bbb918ba877e62b91532644626b9
ltdai010 <47277582+ltdai010@users.noreply.github.com>
2019-03-28 23:11:33 +0700

    Update Sprite.h

--- Sprite.h
+++ Sprite.h
@@ -7,6 +7,9 @@ public:
 	void Draw();
 	void SetX(int x);
 	void SetY(int y);
+	void SetAmountFrame(int x, int y);
+	void SetCurrentFrame(int x);
+	void PlayAnimation(int beginFrame, int endFrame, int row, float speed);
 	int GetX();
 	int GetY();
 private:
@@ -14,5 +17,12 @@ private:
 	SDL_Renderer *renderer;
 	SDL_Texture* image;
 	SDL_Rect rect;
+	SDL_Rect crop;
+	int img_width;
+	int img_height;
+	int amount_Frame_X;
+	int amount_Frame_Y;
+	long long int animationDelay;
+	int currentFrame;
 };
 
