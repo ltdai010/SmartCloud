80511df021a723e2a4181671b78901bb072e25d0
ltdai010 <47277582+ltdai010@users.noreply.github.com>
2019-03-30 22:17:36 +0700

    Update SmartCloud.cpp

--- SmartCloud.cpp
+++ SmartCloud.cpp
@@ -7,18 +7,8 @@ int main(int argc, char* argv[])
 		SDL_Log("Unable to initialize SDL: %s", SDL_GetError());
 		return 1;
 	}
-	bool* quit = false;
-	Game_setup* game_setup = new Game_setup(quit, SCREEN_WIDTH, SCREEN_HEIGHT);
-	Sprite* background = new Sprite(game_setup, "background.bmp", 0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
-	Cloud* cloud = new Cloud(game_setup, "cloud.png", 200, 400, CLOUD_WIDTH, CLOUD_HEIGHT);
-	while (!quit && game_setup->GetMainEvent()->type != SDL_QUIT)
-	{
-		game_setup->Begin();
-		background->Draw();
-		cloud->Movement();
-		cloud->Draw();
-		game_setup->End();
-	}
-	delete game_setup;
+	Main *main = new Main(SCREEN_WIDTH, SCREEN_HEIGHT, CLOUD_WIDTH, CLOUD_HEIGHT);
+	main->GameLoop();
+	delete main;
 	return 0;
 }
