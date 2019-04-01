e2618ca9c3140f72c21ce315af34714652aa7f3c
ltdai010 <47277582+ltdai010@users.noreply.github.com>
2019-04-01 22:17:07 +0700

    Update Main.h

--- Main.h
+++ Main.h
@@ -5,12 +5,19 @@ public:
 	Main(int screenWidth, int screenHeight, int cloudWidth, int cloudHeight);
 	~Main();
 	void GameLoop();
+	void FirstSetup();
+	bool EatenBrain(int i);
+	bool MissedBrain(int i);
+	void UpdateCondition();
+	void EatBook();
 private:
 	Game_setup *game_setup;
+	Game_Text *score_text;
 	Sprite *background;
 	Cloud *cloud;
 	Food *food;
 	bool quit;
+	Uint32 score;
 	long long int timeCheck;
 };
 
