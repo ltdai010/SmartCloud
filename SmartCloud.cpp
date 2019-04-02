41c18626cf05c5bf4885619a62afb9a41a4168fe
ltdai010 <47277582+ltdai010@users.noreply.github.com>
2019-04-02 19:42:43 +0700

    Update SmartCloud.cpp

--- SmartCloud.cpp
+++ SmartCloud.cpp
@@ -7,8 +7,14 @@ int main(int argc, char* argv[])
 		SDL_Log("Unable to initialize SDL: %s", SDL_GetError());
 		return 1;
 	}
+	if (TTF_Init() == -1)
+	{
+		SDL_Log("Unable to initialize TTF: %s", TTF_GetError());
+	}
 	Main *main = new Main(SCREEN_WIDTH, SCREEN_HEIGHT, CLOUD_WIDTH, CLOUD_HEIGHT);
 	main->GameLoop();
 	delete main;
+	TTF_Quit();
+	SDL_Quit();
 	return 0;
 }
