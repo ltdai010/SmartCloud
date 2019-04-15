32b5b038afd1a577f13e3e1b7fe5e1695f087aae
ltdai010 <47277582+ltdai010@users.noreply.github.com>
2019-04-15 23:20:14 +0700

    Update Menu.h

--- Menu.h
+++ Menu.h
@@ -4,7 +4,10 @@ class Menu
 public:
 	Menu(Game_setup *passed_game_setup, std::string path);
 	void Draw();
+	void DrawText();
+	void PrintInstruction();
 	void CheckCommand(Selection *select);
+	void CheckBack(Selection *select);
 	~Menu();
 private:
 	Sprite *menu;
@@ -13,6 +16,11 @@ private:
 	Game_Text *instruction;
 	Game_Text *highScore;
 	Game_Text *quit;
+	Game_Text *instructionText1;
+	Game_Text *instructionText2;
+	Game_Text *instructionText3;
+	Game_Text *back;
+	Sprite *instructionBackground;
 	int mousePointX;
 	int mousePointY;
 };
