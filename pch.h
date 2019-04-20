2c4c97f4e7f2d91b49fe7a50e54f36eefcba162c
ltdai010 <47277582+ltdai010@users.noreply.github.com>
2019-04-20 23:10:08 +0700

    Update pch.h

--- pch.h
+++ pch.h
@@ -11,22 +11,37 @@
 #define SCREEN_WIDTH 1081
 #define SCREEN_HEIGHT 600
 #define AMOUT_BRAIN 10
+#define AMOUT_THREAT 10
+#define CLOUD_START_X 200
+#define CLOUD_START_Y 450
 #define CLOUD_WIDTH 100
 #define CLOUD_HEIGHT 100
 #define BRAIN_WIDTH 50
 #define BRAIN_HEIGHT 50
+#define THREAT_WIDTH 50
+#define THREAT_HEIGHT 50
 #define ClOUD_SPEED 3
 #define BRAIN_SPEED 2
 #define TIME_SPACE 10 
 #define CLOUD_FRAME_X 2
 #define CLOUD_FRAME_Y 2
 #define BORDER_LEFT 100
-#define BORDER_RIGHT 500
+#define BORDER_RIGHT 800
 #define SPAWN_TIME_SPACE 1000
 #define SCORE_PER_BRAIN 10
 #define MENU_TEXT_X 850
 #define START_TEXT_WIDTH 200
 #define START_TEXT_HEIGHT 100
+#define X_SIZE_WIDTH 40
+#define X_SIZE_HEIGHT 40
+#define X_LOCATION_X 1000
+#define X_LOCATION_Y 20
+#define HEALTH_POINT 3
+#define HEART_WIDTH 40
+#define HEART_HEIGHT 40
+#define HEART_X 800
+#define HEART_Y 20
+#define SLOW_TIME 5000
 enum BookType
 {
 	SIZE,
@@ -53,11 +68,12 @@ enum Selection
 #include <cstdlib>
 #include "Game_setup.h"
 #include "Sprite.h"
+#include "Game_Text.h"
 #include "Cloud.h"
 #include "Brain.h"
 #include "Book.h"
 #include "Food.h"
-#include "Game_Text.h"
+#include "Threat.h"
 #include "Menu.h"
 #include "Main.h"
 
