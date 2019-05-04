770a552c82a6c2af785e874fd30ff8aa59bd0e8f
ltdai010 <47277582+ltdai010@users.noreply.github.com>
2019-05-04 22:16:19 +0700

    Update Book.cpp

--- Book.cpp
+++ Book.cpp
@@ -2,10 +2,10 @@
 #include "Book.h"
 
 
-Book::Book(Game_setup *passed_game_setup, std::string path, int x, int y, int w, int h, BookType type)
+Book::Book(Game_setup *passed_game_setup, std::string path, int x, int y, int w, int h)
 {
 	book = new Sprite(passed_game_setup->GetRenderer(), path, x, y, w, h);
-	bookType = type;
+	book->SetAmountFrame(3, 1);
 }
 
 
@@ -18,9 +18,34 @@ void Book::MoveDown()
 {
 	if (timeCheck + TIME_SPACE < SDL_GetTicks())
 	{
-		book->SetX(book->GetX() - ClOUD_SPEED * 2);
+		book->SetY(book->GetY() + BRAIN_SPEED);
 		timeCheck = SDL_GetTicks();
 	}
+}
+
+void Book::SetType(BookType type)
+{
+	bookType = type;
+	if (type == SIZE)
+	{
+		book->SetCurrentFrame(2);
+		book->PlayAnimation(2, 2, 0, 0);
+	}
+	else if (type == LIFE)
+	{
+		book->SetCurrentFrame(1);
+		book->PlayAnimation(1, 1, 0, 0);
+	}
+	else if (type == DOUBLE_SCORE)
+	{
+		book->SetCurrentFrame(0);
+		book->PlayAnimation(0, 0, 0, 0);
+	}
+}
+
+BookType Book::GetBookType()
+{
+	return bookType;
 }
 
 void Book::Draw()
@@ -28,4 +53,22 @@ void Book::Draw()
 	book->Draw();
 }
 
+void Book::SetX(int x)
+{
+	book->SetX(x);
+}
 
+void Book::SetY(int y)
+{
+	book->SetY(y);
+}
+
+int Book::GetX()
+{
+	return book->GetX();
+}
+
+int Book::GetY()
+{
+	return book->GetY();
+}
