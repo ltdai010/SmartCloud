fdf8edc4979e466baf35f9207757c9d31612474f
ltdai010 <47277582+ltdai010@users.noreply.github.com>
2019-05-04 22:17:59 +0700

    Update Food.h

--- Food.h
+++ Food.h
@@ -5,16 +5,22 @@ public:
 	Food(Game_setup *passed_game_setup);
 	~Food();
 	void RandomSpawnFood(int i);
+	void RandomBook();
 	void Movement();
-	void SpawnFood(int i);
+	void SpawnBrain(int i);
+	void SpawnBook();
 	void Draw();
 	int GetBrainX(int i);
 	int GetBrainY(int i);
-
+	BookType GetBookType();
+	int GetBookX();
+	int GetBookY();
 private:
 	Brain* brain[AMOUT_BRAIN];
+	Book* book;
 	Game_setup* game_setup;
-	int spawnLocationY[AMOUT_BRAIN];
-	int spawnLocationX[AMOUT_BRAIN];
+	int spawnLocationY[AMOUT_BRAIN + 1];
+	int spawnLocationX[AMOUT_BRAIN + 1];
+	int type;
 };
 
